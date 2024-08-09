58452030f64e28f694e98771d29027a9f0027b60
Suki <tues1205@naver.com>
2024-08-09 22:59:15 +0900

    가위바위보 게임

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import logo from './logo.svg';
+import scissors from './media/scissors.png'
+import rock from './media/rock.png'
+import paper from './media/paper.png'
+
 import { useState } from 'react';
 import Box from './component/Box';
 import './App.css';
@@ -14,15 +17,15 @@ import './App.css';
 const choice = {
   scissors: {
     name:"Scissors",
-    img:"https://search.pstatic.net/sunny/?src=https%3A%2F%2Fimg.lovepik.com%2Ffree-png%2F20211212%2Flovepik-scissors-png-image_401529665_wh1200.png&type=sc960_832"
+    img: scissors
   },
   rock: {
     name:"Rock",
-    img:"https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Fe1%2Fb7%2Fb8%2Fe1b7b8012277d7c68c7e9b68d66ac02d.jpg&type=sc960_832"
+    img: rock
   },
   paper: {
     name:"Paper",
-    img:"https://search.pstatic.net/sunny/?src=https%3A%2F%2Fpng.pngtree.com%2Felement_our%2F20200702%2Fourlarge%2Fpngtree-white-nailed-paper-image_2286565.jpg&type=sc960_832"
+    img: paper
   }
 }
 
@@ -30,6 +33,7 @@ function App() {
 
   const [userSelect, setUserSelect] = useState(null); //나의 ui를 바꾸는 state
   const [comSelect, setComSelect] = useState(null); // 컴퓨터의 ui를 바꾸는 state
+  const [result, setResult] = useState(""); // 결과 ui를 바꾸는 state
 
   const play =(userChoice)=> {
     // You
@@ -39,7 +43,27 @@ function App() {
     // Computer
     let comChoice = randomChoice();
     setComSelect(choice[comChoice]);
+
+    // 승패 판정 (You 기준 이기면 true)
+    setResult(judgement(choice[userChoice], choice[comChoice]));
   };
+
+  const judgement =(user, com)=> {
+
+    if (user.name == com.name) {
+      return "Tie"
+    }
+    else if (user.name == "Rock") {
+      return com.name=="Scissors"?"Win":"Lose"
+    }
+    else if (user.name == "Scissors") {
+      return com.name=="Paper"?"Win":"Lose"
+    }
+    else if (user.name == "Paper") {
+      return com.name=="Rock"?"Win":"Lose"
+    }
+
+  }
 
   const randomChoice =()=> {
   
@@ -55,19 +79,21 @@ function App() {
   return (
     <div>
       <div className='main'>
-      <Box title='You' item={userSelect}/>
-      <Box title='Computer' item={comSelect}/>
+      <Box title='You' item={userSelect} result={result}/>
+      <Box title='Computer' item={comSelect} result={result}/>
       </div>
     
       <div className='main'>
-        <button onClick={() => play("scissors")}>가위</button>
-        <button onClick={() => play("rock")}>바위</button>
-        <button onClick={() => play("paper")}>보</button>
+        <button id="btn" onClick={() => play("scissors")}>가위</button>
+        <button id="btn" onClick={() => play("rock")}>바위</button>
+        <button id="btn" onClick={() => play("paper")}>보</button>
+      </div>
+
+      <div className='main'>
+        <h1>{result}</h1>
       </div>
 
     </div>
-    
-
     
   );
 }
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,14 +1,23 @@
 import React from 'react'
 
 const Box = (props) => {
+
+  let result; // 결과
+  if (props.title == "Computer" && props.result != "Tie" && props.result != "") {
+    result = props.result == "Win" ? "Lose" : "Win" // 컴퓨터는 결과의 반대로 출력
+  }
+  else {
+    result = props.result
+  }
+
   return (
-    <div className='box'>
+    <div className={`box ${result}`}>
         <h1>{props.title}</h1>
 
         {/* 처음에는 img가 null이기 때문에 에러 방지로 props.item이 true일 때만 이미지 출력하도록 */}
         <img className='item-img' src={props.item && props.item.img}/>
 
-        <h2>WIN</h2>
+        <h2>{result}</h2>
     </div>
   );
 }
