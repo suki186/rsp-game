3d575c1a5517c7a0a927272110ced749dc71e44b
Suki <tues1205@naver.com>
2024-09-20 18:54:49 +0900

    mobile css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import scissors from './media/scissors.png'
-import rock from './media/rock.png'
-import paper from './media/paper.png'
-import scissorsBtn from './media/scissorsBtn.png'
-import rockBtn from './media/rockBtn.png'
-import paperBtn from './media/paperBtn.png'
+import scissors from "./media/scissors.png";
+import rock from "./media/rock.png";
+import paper from "./media/paper.png";
+import scissorsBtn from "./media/scissorsBtn.png";
+import rockBtn from "./media/rockBtn.png";
+import paperBtn from "./media/paperBtn.png";
 
-import { useState } from 'react';
-import Box from './component/Box';
-import './App.css';
-import Score from './component/Score';
-
+import { useState } from "react";
+import Box from "./component/Box";
+import "./App.css";
+import Score from "./component/Score";
 
 //1. 박스 2개 (타이틀, 사진, 결과)
 //2. 가위바위보 버튼 3개
@@ -21,27 +20,26 @@ import Score from './component/Score';
 // 사진
 const choice = {
   scissors: {
-    name:"Scissors",
-    img: scissors
+    name: "Scissors",
+    img: scissors,
   },
   rock: {
-    name:"Rock",
-    img: rock
+    name: "Rock",
+    img: rock,
   },
   paper: {
-    name:"Paper",
-    img: paper
-  }
-}
+    name: "Paper",
+    img: paper,
+  },
+};
 
 function App() {
-
   const [userSelect, setUserSelect] = useState(null); //나의 ui를 바꾸는 state
   const [comSelect, setComSelect] = useState(null); // 컴퓨터의 ui를 바꾸는 state
   const [result, setResult] = useState("Ready"); // 결과 ui를 바꾸는 state
   const [score, setScore] = useState(0); // 점수 ui를 바꾸는 state
 
-  const play =(userChoice)=> {
+  const play = (userChoice) => {
     // You
     console.log("선택됨: ", userChoice);
     setUserSelect(choice[userChoice]); // choice 목록 중 button에서 클릭한 내용을 userSelect로 설정
@@ -51,63 +49,60 @@ function App() {
     setComSelect(choice[comChoice]);
 
     // 승패 판정 (You 기준 이기면 true)
-    const j = judgement(choice[userChoice], choice[comChoice])
+    const j = judgement(choice[userChoice], choice[comChoice]);
     setResult(j);
 
-    if (j == "Win") {
+    if (j === "Win") {
       setScore(score + 1);
     }
-    
   };
 
-  const judgement =(user, com)=> {
+  const judgement = (user, com) => {
+    if (user.name === com.name) {
+      return "Tie";
+    } else if (user.name === "Rock") {
+      return com.name === "Scissors" ? "Win" : "Lose";
+    } else if (user.name === "Scissors") {
+      return com.name === "Paper" ? "Win" : "Lose";
+    } else if (user.name === "Paper") {
+      return com.name === "Rock" ? "Win" : "Lose";
+    }
+  };
 
-    if (user.name == com.name) {
-      return "Tie"
-    }
-    else if (user.name == "Rock") {
-      return com.name=="Scissors"?"Win":"Lose"
-    }
-    else if (user.name == "Scissors") {
-      return com.name=="Paper"?"Win":"Lose"
-    }
-    else if (user.name == "Paper") {
-      return com.name=="Rock"?"Win":"Lose"
-    }
-
-  }
-
-  const randomChoice =()=> {
-  
+  const randomChoice = () => {
     const itemArray = Object.keys(choice); // 객체의 key값만 뽑아 배열화
     console.log("item array", itemArray);
 
-    const comItem = itemArray[Math.floor(Math.random()*itemArray.length)]; //0(가위), 1(바위), 2(보) -> 인덱스에 맞는 이름
+    const comItem = itemArray[Math.floor(Math.random() * itemArray.length)]; //0(가위), 1(바위), 2(보) -> 인덱스에 맞는 이름
     console.log("com: ", comItem);
 
     return comItem;
   };
 
   return (
-    <div className='background'>
-      <div className='main'>
-        <h1 id='h'>{result}</h1>
-            
-        <div className='container'>
-          <Box title='You' item={userSelect} result={result}/>
-          <Score num={score}/>
-          <Box title='Com' item={comSelect} result={result}/>
-        </div>
-        
-        <div className='choice'>
-          <button className='btn' onClick={() => play("scissors")}><img src={scissorsBtn}/></button>
-          <button className="btn" onClick={() => play("rock")}><img src={rockBtn}/></button>
-          <button className="btn" onClick={() => play("paper")}><img src={paperBtn} /></button>
+    <div className="background">
+      <div className="main">
+        <h1 id="h">{result}</h1>
+
+        <div className="container">
+          <Box title="You" item={userSelect} result={result} />
+          <Score num={score} />
+          <Box title="Com" item={comSelect} result={result} />
         </div>
 
+        <div className="choice">
+          <button className="btn" onClick={() => play("scissors")}>
+            <img src={scissorsBtn} alt="sciccsor-img" />
+          </button>
+          <button className="btn" onClick={() => play("rock")}>
+            <img src={rockBtn} alt="rock-img" />
+          </button>
+          <button className="btn" onClick={() => play("paper")}>
+            <img src={paperBtn} alt="paper-img" />
+          </button>
+        </div>
       </div>
     </div>
-    
   );
 }
 
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React from "react";
+import noting from "../media/noting.png";
 
 const Box = (props) => {
-
   let result; // 결과
-  if (props.title == "Com" && props.result != "Tie" && props.result != "Ready") {
-    result = props.result == "Win" ? "Lose" : "Win" // 컴퓨터는 결과의 반대로 출력
-  }
-  else {
-    result = props.result
+  if (
+    props.title === "Com" &&
+    props.result !== "Tie" &&
+    props.result !== "Ready"
+  ) {
+    result = props.result === "Win" ? "Lose" : "Win"; // 컴퓨터는 결과의 반대로 출력
+  } else {
+    result = props.result;
   }
 
   return (
     <div className={`box ${result}`}>
-        <h1>{props.title}</h1>
+      <h1>{props.title}</h1>
 
-        {/* 처음에는 img가 null이기 때문에 에러 방지로 props.item이 true일 때만 이미지 출력하도록 */}
-        <img className='item-img' src={props.item && props.item.img}/>
+      {/* 처음에는 img가 null이기 때문에 에러 방지로 props.item이 true일 때만 이미지 출력하도록 */}
+      {props.item ? (
+        <img className="item-img" src={props.item.img} alt="img" />
+      ) : (
+        <img className="item-img" src={noting} alt="img" />
+      )}
 
-        <h2>{result}</h2>
+      <h2>{result}</h2>
     </div>
   );
-}
+};
 
-export default Box
+export default Box;
